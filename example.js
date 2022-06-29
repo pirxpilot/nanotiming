7aae77249ead8267cc035321514536ae463a2ff1
Damian Krzeminski <pirxpilot@furkot.com>
2022-06-29 13:09:13 +0200

    modernize code

--- example.js
+++ example.js
@@ -1,21 +1,20 @@
-var nanotiming = require('./')
+const nanotiming = require('./')
 
 try {
-  var hooks = require('perf_hooks')
-  var nanot = nanotiming('my-loop') // Start profiling
+  const hooks = require('perf_hooks')
+  const nanot = nanotiming('my-loop') // Start profiling
+
+  let i = 10
+  while (--i) console.log(i)
+
+  // Stop profiling
+  nanot()
+
+  const timings = hooks.performance.getEntries()
+  const timing = timings[timings.length - 1]
+  console.log(timing.name, timing.duration) // log the last entry
+  hooks.performance.clearMeasures(timing.name) // be a good citizen and free after use
 } catch (e) {
   console.log('perf_hooks not available, exiting')
   process.exit(1)
 }
-var performance = hooks.performance
-
-var i = 10
-while (--i) console.log(i)
-
-// Stop profiling
-nanot()
-
-var timings = performance.getEntries()
-var timing = timings[timings.length - 1]
-console.log(timing.name, timing.duration) // log the last entry
-performance.clearMeasures(timing.name)    // be a good citizen and free after use
